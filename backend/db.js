560da8c6e23f35852810f5961ccf67f2a735921f
bypipe <felipe.calan@correounivalle.edu.co>
2025-11-09 23:02:25 -0500

    conexion a bd en render para despliegue

--- backend/db.js
+++ backend/db.js
@@ -1,17 +1,45 @@
 
+// const { Pool } = require("pg");
+// require("dotenv").config();
+
+// const pool = new Pool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT,
+// });
+
+// pool.connect()
+//   .then(() => console.log("✅ Conexión exitosa a PostgreSQL"))
+//   .catch(err => console.error("❌ Error al conectar con la base de datos", err));
+
+// module.exports = pool;
+
+
 const { Pool } = require("pg");
 require("dotenv").config();
 
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-});
+const isRender = !!process.env.DATABASE_URL;
 
-pool.connect()
+const pool = new Pool(
+  isRender
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false } // Render exige conexión segura
+      }
+    : {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        port: process.env.DB_PORT
+      }
+);
+
+pool
+  .connect()
   .then(() => console.log("✅ Conexión exitosa a PostgreSQL"))
-  .catch(err => console.error("❌ Error al conectar con la base de datos", err));
+  .catch((err) => console.error("❌ Error al conectar con la base de datos", err));
 
 module.exports = pool;
